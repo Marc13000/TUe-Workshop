4039c64eb76d36d51db870ff93cd9f7653ebe2f6
Jaiditya Khemani <51130635+Marc13000@users.noreply.github.com>
2025-03-18 14:49:28 +0100

    Update tests.kt

--- tests.kt
+++ tests.kt
@@ -11,6 +11,11 @@ fun main() {
     assertEquals(10, sumOfDigits(1234))
     assertEquals(30, sumOfDigits(9876))
     assertEquals(6, sumOfDigits(123))
+
+    // Test factorial
+    assertEquals(120, factorial(5))
+    assertEquals(5040, factorial(7))
+    assertEquals(1, factorial(1))
     
     println("All tests passed!")
 }
