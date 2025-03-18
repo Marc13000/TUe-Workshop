5661f557e803ab009a50b870170fbf61c114393f
Jaiditya Khemani <51130635+Marc13000@users.noreply.github.com>
2025-03-18 14:43:34 +0100

    Update tests.kt

--- tests.kt
+++ tests.kt
@@ -6,6 +6,11 @@ fun main() {
     assertEquals(3, countVowels("Hello World"))
     assertEquals(4, countVowels("Kotlin is fun!"))
     assertEquals(5, countVowels("Education is key"))
+
+    // Test sumOfDigits
+    assertEquals(10, sumOfDigits(1234))
+    assertEquals(30, sumOfDigits(9876))
+    assertEquals(6, sumOfDigits(123))
     
     println("All tests passed!")
 }
